41663d5b5a00b37420cef296253540818d7e8984
Alvaro Cueva <alvmantura23@gmail.com>
2025-03-22 05:13:04 -0500

    Reestructuracion de carpetas

---
+++ scripts/Index.js
@@ -0,0 +1,13 @@
+"use strict"
+function redirectToInfoProductoA() {
+    window.location.href = "infoProductoA.html";
+}
+function redirectToInfoProductoB() {
+    window.location.href = "infoProductoB.html";
+}
+function redirectToInfoProductoC() {
+    window.location.href = "infoProductoC.html";
+}
+function redirectToInfoProductoD() {
+    window.location.href = "infoProductoD.html";
+}
